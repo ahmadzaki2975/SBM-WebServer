3573d0a021f7977acd84056f210f101f78f3ac9c
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2023-05-21 12:13:02 +0700

    fix: identity

--- sbm-webserver/sbm-webserver.ino
+++ sbm-webserver/sbm-webserver.ino
@@ -1,3 +1,11 @@
+// ESP32 WebServer
+// Oleh:
+// Agustinus Angelo Christian Fernando - 21/473804/TK/52235
+// Aufa Nasywa Rahman - 21/475255/TK/52454
+// Giga Hidjrika Aura Adkhy - 21/479228/TK/52833
+// Difta Fitrahul Qihaj - 21/480096/TK/52975
+// Ahmad Zaki Akmal - 21/480179/TK/52981
+
 // Import all library
 #include <WiFi.h>
 #include <AsyncTCP.h>
